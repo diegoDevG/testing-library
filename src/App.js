bf0d8fa09c40d051f7ac2b7af744866188bbe45c
Diego Andres Cardenas <diego.cardenas@globant.com>
2021-06-20 22:07:07 -0500

    Add desable button from checkbox

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 function App() {
 
   const [buttonColor, setButtonColor] = useState('red')
+  const [disabled, setDisabled] = useState(false)
+
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
   return (
     <div className="App">
       <button
         style={{ backgroundColor: buttonColor }}
+        disabled = {disabled}
         onClick={() => {setButtonColor(newButtonColor)}}
       >
         Change to { newButtonColor }
-     </button>
+      </button>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        aria-checked={disabled}
+        defaultChecked={disabled}
+        onClick={() => setDisabled(!disabled)} />
+     
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,50 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial text and color', () => {
-  render(<App />);
-  const colorButton = screen.getByRole('button', { name: /change to blue/i })
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+describe('render a button in the page', () => {
   
-  //click button
-  fireEvent.click(colorButton)
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
-  
-  //expect the button text to be 'Change to red'
-  expect(colorButton.textContent).toBe('Change to red')
+  it('button is in the page', () => {
+    render(<App />);
+    const colorButton = screen.getByRole('button', { name: /change to blue/i })
+    expect(colorButton).toBeInTheDocument()
+  })
+
+  it('render an enabled button and no checked checkbox', () => {
+    render(<App />)
+    const colorButton = screen.getByRole('button', { name: /change to blue/i })
+    expect(colorButton).toBeEnabled()
+    
+    const checkbox = screen.getByRole('checkbox')
+    expect(checkbox).not.toBeChecked()
+  })
+
+  it('button has correct initial text and color', () => {
+    render(<App />);
+    const colorButton = screen.getByRole('button', { name: /change to blue/i })
+    expect(colorButton).toHaveStyle({ backgroundColor: 'red' })    
+  })
+
+  it('button change color and text on click', () => {
+    render(<App />)
+    const colorButton = screen.getByRole('button', { name: /change to blue/i })
+    
+    fireEvent.click(colorButton)
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+    
+    expect(colorButton.textContent).toBe('Change to red')
+  })
+
+  test('Checkbox disables button on first click and enables on second click', () => {
+    render(<App />)
+    const checkbox = screen.getByRole('checkbox')
+    const button = screen.getByRole('button')
+
+    fireEvent.click(checkbox)
+    expect(button).toBeDisabled()
+
+    fireEvent.click(checkbox)
+    expect(button).toBeEnabled()
+
+  })
 })
+
